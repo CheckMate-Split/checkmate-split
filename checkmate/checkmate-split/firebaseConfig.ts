553bb043c56c1e9398b798505cbe8646174dd4be
brandon-ring <60483784+brandon-ring@users.noreply.github.com>
2025-07-14 11:41:21 -0400

    Fix Firebase re-initialization

--- checkmate/checkmate-split/firebaseConfig.ts
+++ checkmate/checkmate-split/firebaseConfig.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
@@ -14,7 +14,7 @@ const firebaseConfig = {
   measurementId: "G-EYWPP5FDK6"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 

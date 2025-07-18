f12e4464daa43971f888388d45adf4a43776ea2c
brandon-ring <60483784+brandon-ring@users.noreply.github.com>
2025-07-18 08:48:50 -0400

    Update Firebase storage bucket

--- checkmate/checkmate-split/firebaseConfig.ts
+++ checkmate/checkmate-split/firebaseConfig.ts
@@ -12,7 +12,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAjcwRdBBQ3PdjiXHIIgsmJj_B8Jw70-Do",
   authDomain: "checkmate-split-app.firebaseapp.com",
   projectId: "checkmate-split-app",
-  storageBucket: "checkmate-split-app.appspot.com",
+  storageBucket: "checkmate-split-app.firebasestorage.app",
   messagingSenderId: "638783503585",
   appId: "1:638783503585:web:651a5ad7109d7bbf3c0ba0",
   measurementId: "G-EYWPP5FDK6"
